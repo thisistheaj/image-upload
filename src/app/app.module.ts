8b57388ffd9fcfc95ede239d5fd490589f11ecf0
thisistheaj <ajbeckner@gmail.com>
2018-01-25 19:21:24 -0500

    foo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {AngularFireModule} from "angularfire2";
+import {AngularFireDatabaseModule} from "angularfire2/database-deprecated";
+import {AngularFireDatabase} from "angularfire2/database";
 
 @NgModule({
   declarations: [
@@ -14,7 +17,16 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCeE1ipn4-WPWwrumBKvBNuCdMx_DytXgI",
+      authDomain: "teamrate-65160.firebaseapp.com",
+      databaseURL: "https://teamrate-65160.firebaseio.com",
+      projectId: "teamrate-65160",
+      storageBucket: "teamrate-65160.appspot.com",
+      messagingSenderId: "88014707684"
+    }),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +36,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireDatabase
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as firebase from 'firebase';
+import { AngularFireDatabase } from "angularfire2/database";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public urls = [];
+
+  constructor(public navCtrl: NavController, public db: AngularFireDatabase) {
 
   }
 
+  public fileChanged(event) {
+    console.log('file changed: ', event);
+    let file = event.target.files[0];
+    // this.db.object('file').set(file.name)
+    //   .then(data => console.log('success: ',data), err => console.error(err))
+    firebase.storage().ref('files/' + file.name)
+      .put(event.target.files[0])
+      .then(data => this.urls.push(data.downloadURL), err => console.error(err))
+  }
 }
